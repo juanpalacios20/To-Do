4be6f24749857077a9255c8f90bb9fcb3cad3f7d
Juan Palacios <juanpalaciosc20@gmail.com>
2024-08-23 20:31:41 -0500

    haciendo correcciones

--- prioridad/models.py
+++ prioridad/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class prioridad (models.Model):
     nombre = models.CharField(max_length=50)
-    nivel = models.IntegerField
+    nivel = models.IntegerField(default=1)
 
     def __str__(self):
         return self.nombre
--- tareas/models.py
+++ tareas/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from estado.models import estado
 from prioridad.models import prioridad
+import datetime
 
 # Create your models here.
 class tareas (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=500)
-    done = models.BooleanField(default=False)
-    Fecha = models.DateField
+    fecha = models.DateField(default=datetime.date.today)
     estado = models.ForeignKey(estado, on_delete=models.CASCADE)
     prioridad = models.ForeignKey(prioridad, on_delete=models.CASCADE)
 

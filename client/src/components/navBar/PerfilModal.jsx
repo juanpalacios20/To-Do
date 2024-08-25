7d6d78d07b0939284daf8d2cd639031ad2300ff9
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-08-25 16:40:47 -0500

    Boton para agregar carpeta

--- client/src/components/navBar/PerfilModal.jsx
+++ client/src/components/navBar/PerfilModal.jsx
@@ -53,6 +53,12 @@ function PerfilModal(props) {
                     </>
                 }
                 {props.children}
+                {props.perfil ? null : <button className="ModalButtonMenuAdd Transition transition-all duration-300 z-50" onClick={''}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-16">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                </button>
+                }
             </dialog>
         )
     } else {

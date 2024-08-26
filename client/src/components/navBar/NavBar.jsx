d1cf1e89db0d7410857aab9cb33f79fba578747e
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-08-25 20:10:49 -0500

    Pequeño cambio en el nav y el enrutamiento para la implementacion de la pagina de perfil

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,10 +1,15 @@
 import DarkMode from "../darkMode/DarkMode"
 import { lazy, Suspense, useState } from "react"
 import NavButton from "./NavButton.jsx"
+import { PropTypes } from 'prop-types'
 
 const LazyModal = lazy(() => import("./PerfilModal.jsx"))
 
-function NavBar() {
+function NavBar(props) {
+
+    NavBar.propTypes = {
+        location: PropTypes.object.isRequired
+    }
 
     const [perfilModal, setPerfilModal] = useState(false)
     const [menuModal, setMenuModal] = useState(false)
@@ -17,12 +22,12 @@ function NavBar() {
         setMenuModal(false)
     }
 
-    console.log(perfilModal)
+    console.log(props.location.pathname)
 
     return (
         <>
             {/* ======================================= NavBar ======================================= */}
-            <div className="NavBarContainer transition-all duration-300">
+            <div className= {props.location.pathname === "/Home" ? "NavBarContainerTasks transition-all duration-300" : `${"NavBarContainerPerfil transition-all duration-300"}`}>
                 <div className="NavBar transition-all duration-300">
                     <div className="NavBarContent">
                         <div onClick={() => setMenuModal(true)} className="NavBarMenu hover:cursor-pointer Transition transition-all duration-300">
@@ -48,12 +53,12 @@ function NavBar() {
                 {/* ======================================= Modales perezosos ======================================= */}
                 <Suspense>
                     <LazyModal perfil state={perfilModal} toggleOff={closePerfilModal} >
-                        <NavButton text="Perfil" redirectTo="perfil">
+                        <NavButton text="Perfil" redirectTo="Perfil">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 mr-2">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                             </svg>
                         </NavButton>
-                        <NavButton text="Cerrar sesión" redirectTo="logion">
+                        <NavButton text="Cerrar sesión" redirectTo="Login">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 mr-2 text-red-500">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
                             </svg>
---
+++ client/src/components/pages/Perfil/Perfil.jsx
@@ -0,0 +1,11 @@
+function Perfil () {
+    return(
+        <div className="PerfilContainer PageContentBack">
+            <div className="Perfil PageContent">
+                <h2 className="PerfilText">Hola</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Perfil

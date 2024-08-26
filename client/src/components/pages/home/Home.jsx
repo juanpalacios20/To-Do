fa4f28ddec97a9d772db1ff03c08862612ca39cf
MiguelRestrepoL <ith1zdragonz@gmail.com>
2024-08-26 11:36:16 -0500

    Testing

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
                 </div>
             </div>
             {/* ======================================= Dark Mode ======================================= */}
-            <LazyModal state={taskModal} toggleOff={closeTaskModal} />
+            <LazyModal tareas state={taskModal} toggleOff={closeTaskModal} />
         </>
     )
 }

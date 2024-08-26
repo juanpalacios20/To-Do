b0445c32e910fbfc086ef33fe9bf257ef218abd0
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-08-25 22:45:52 -0500

    proptypes

--- client/src/components/pages/Perfil/PerfilToast.jsx
+++ client/src/components/pages/Perfil/PerfilToast.jsx
@@ -1,4 +1,12 @@
+import { PropTypes } from 'prop-types'
+
 function PerfilToast(props) {
+
+    PerfilToast.propTypes = {
+        title: PropTypes.string.isRequired,
+        numero: PropTypes.number.isRequired
+    }
+
     return (
         <div className="PerfilContentStatsPendientesContainer flex flex-col py-4 px-4 size-fit rounded-xl transition-all duration-300 rounded-xl">
             <h4>{props.title}</h4>
